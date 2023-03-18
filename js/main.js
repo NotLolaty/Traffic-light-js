0816e162dbfd6dd75f967c48d1a12554fe5370fa
NotLolaty <bmutalov90@gmail.com>
2023-03-19 00:39:51 +0500

    nice traffic lights

---
+++ js/main.js
@@ -0,0 +1,230 @@
+document.getElementById("header__title").style.width = "auto";
+document.getElementById("header__title").style.border = "solid grey";
+document.getElementById("header__title").style.backgroundColor = "grey";
+document.getElementById("header__title").style.borderRadius = "5px";
+document.getElementById("header__title").style.textAlign = "center";
+document.getElementById("header__title").style.color = "white";
+document.getElementById("header__title").style.marginLeft = "auto";
+document.getElementById("header__title").style.marginRight = "auto";
+
+document.getElementById("main").style.backgroundColor = "grey";
+document.getElementById("main").style.width = "250px";
+document.getElementById("main").style.marginLeft = "500px";
+document.getElementById("main").style.borderRadius = "5px";
+document.getElementById("main").style.marginLeft = "auto";
+document.getElementById("main").style.marginRight = "auto";
+document.getElementById("main").style.padding = "5px";
+
+document.getElementById("main__red-light").style.border = "solid, red";
+document.getElementById("main__red-light").style.backgroundColor = "red";
+document.getElementById("main__red-light").style.borderRadius = "50%";
+document.getElementById("main__red-light").style.textAlign = "center";
+document.getElementById("main__red-light").style.width = "150px";
+document.getElementById("main__red-light").style.height = "150px";
+document.getElementById("main__red-light").style.marginLeft = "auto";
+document.getElementById("main__red-light").style.marginRight = "auto";
+
+// yellow
+
+document.getElementById("main__yellow-light").style.border = "solid, yellow";
+document.getElementById("main__yellow-light").style.backgroundColor = "grey";
+document.getElementById("main__yellow-light").style.borderRadius = "50%";
+document.getElementById("main__yellow-light").style.textAlign = "center";
+document.getElementById("main__yellow-light").style.width = "150px";
+document.getElementById("main__yellow-light").style.height = "150px";
+document.getElementById("main__yellow-light").style.marginLeft = "auto";
+document.getElementById("main__yellow-light").style.marginRight = "auto";
+
+setTimeout(() => {
+  document.getElementById("main__red-light").style.border = "solid, red";
+  document.getElementById("main__red-light").style.backgroundColor = "grey";
+  document.getElementById("main__red-light").style.borderRadius = "50%";
+  document.getElementById("main__red-light").style.textAlign = "center";
+  document.getElementById("main__red-light").style.width = "150px";
+  document.getElementById("main__red-light").style.height = "150px";
+  document.getElementById("main__red-light").style.marginLeft = "auto";
+  document.getElementById("main__red-light").style.marginRight = "auto";
+
+  document.getElementById("main__yellow-light").style.border = "solid, yellow";
+  document.getElementById("main__yellow-light").style.backgroundColor =
+    "yellow";
+  document.getElementById("main__yellow-light").style.borderRadius = "50%";
+  document.getElementById("main__yellow-light").style.textAlign = "center";
+  document.getElementById("main__yellow-light").style.width = "150px";
+  document.getElementById("main__yellow-light").style.height = "150px";
+  document.getElementById("main__yellow-light").style.marginLeft = "auto";
+  document.getElementById("main__yellow-light").style.marginRight = "auto";
+}, 5000);
+
+setTimeout(() => {
+  document.getElementById("main__yellow-light").style.border = "solid, yellow";
+  document.getElementById("main__yellow-light").style.backgroundColor = "grey";
+  document.getElementById("main__yellow-light").style.borderRadius = "50%";
+  document.getElementById("main__yellow-light").style.textAlign = "center";
+  document.getElementById("main__yellow-light").style.width = "150px";
+  document.getElementById("main__yellow-light").style.height = "150px";
+  document.getElementById("main__yellow-light").style.marginLeft = "auto";
+  document.getElementById("main__yellow-light").style.marginRight = "auto";
+}, 5500);
+
+setTimeout(() => {
+  document.getElementById("main__yellow-light").style.border = "solid, yellow";
+  document.getElementById("main__yellow-light").style.backgroundColor =
+    "yellow";
+  document.getElementById("main__yellow-light").style.borderRadius = "50%";
+  document.getElementById("main__yellow-light").style.textAlign = "center";
+  document.getElementById("main__yellow-light").style.width = "150px";
+  document.getElementById("main__yellow-light").style.height = "150px";
+  document.getElementById("main__yellow-light").style.marginLeft = "auto";
+  document.getElementById("main__yellow-light").style.marginRight = "auto";
+}, 6000);
+
+setTimeout(() => {
+  document.getElementById("main__yellow-light").style.border = "solid, yellow";
+  document.getElementById("main__yellow-light").style.backgroundColor = "grey";
+  document.getElementById("main__yellow-light").style.borderRadius = "50%";
+  document.getElementById("main__yellow-light").style.textAlign = "center";
+  document.getElementById("main__yellow-light").style.width = "150px";
+  document.getElementById("main__yellow-light").style.height = "150px";
+  document.getElementById("main__yellow-light").style.marginLeft = "auto";
+  document.getElementById("main__yellow-light").style.marginRight = "auto";
+}, 6500);
+
+setTimeout(() => {
+  document.getElementById("main__yellow-light").style.border = "solid, yellow";
+  document.getElementById("main__yellow-light").style.backgroundColor =
+    "yellow";
+  document.getElementById("main__yellow-light").style.borderRadius = "50%";
+  document.getElementById("main__yellow-light").style.textAlign = "center";
+  document.getElementById("main__yellow-light").style.width = "150px";
+  document.getElementById("main__yellow-light").style.height = "150px";
+  document.getElementById("main__yellow-light").style.marginLeft = "auto";
+  document.getElementById("main__yellow-light").style.marginRight = "auto";
+}, 7000);
+
+document.getElementById("main__green-light").style.border = "solid, green";
+document.getElementById("main__green-light").style.backgroundColor = "grey";
+document.getElementById("main__green-light").style.borderRadius = "50%";
+document.getElementById("main__green-light").style.textAlign = "center";
+document.getElementById("main__green-light").style.width = "150px";
+document.getElementById("main__green-light").style.height = "150px";
+document.getElementById("main__green-light").style.marginLeft = "auto";
+document.getElementById("main__green-light").style.marginRight = "auto";
+
+// green
+
+setTimeout(() => {
+  document.getElementById("main__yellow-light").style.border = "solid, yellow";
+  document.getElementById("main__yellow-light").style.backgroundColor = "grey";
+  document.getElementById("main__yellow-light").style.borderRadius = "50%";
+  document.getElementById("main__yellow-light").style.textAlign = "center";
+  document.getElementById("main__yellow-light").style.width = "150px";
+  document.getElementById("main__yellow-light").style.height = "150px";
+  document.getElementById("main__yellow-light").style.marginLeft = "auto";
+  document.getElementById("main__yellow-light").style.marginRight = "auto";
+
+  document.getElementById("main__green-light").style.border = "solid, green";
+  document.getElementById("main__green-light").style.backgroundColor = "green";
+  document.getElementById("main__green-light").style.borderRadius = "50%";
+  document.getElementById("main__green-light").style.textAlign = "center";
+  document.getElementById("main__green-light").style.width = "150px";
+  document.getElementById("main__green-light").style.height = "150px";
+  document.getElementById("main__green-light").style.marginLeft = "auto";
+  document.getElementById("main__green-light").style.marginRight = "auto";
+}, 10000);
+
+setTimeout(() => {
+  document.getElementById("main__green-light").style.border = "solid, green";
+  document.getElementById("main__green-light").style.backgroundColor = "grey";
+  document.getElementById("main__green-light").style.borderRadius = "50%";
+  document.getElementById("main__green-light").style.textAlign = "center";
+  document.getElementById("main__green-light").style.width = "150px";
+  document.getElementById("main__green-light").style.height = "150px";
+  document.getElementById("main__green-light").style.marginLeft = "auto";
+  document.getElementById("main__green-light").style.marginRight = "auto";
+}, 10500);
+
+setTimeout(() => {
+  document.getElementById("main__green-light").style.border = "solid, green";
+  document.getElementById("main__green-light").style.backgroundColor = "green";
+  document.getElementById("main__green-light").style.borderRadius = "50%";
+  document.getElementById("main__green-light").style.textAlign = "center";
+  document.getElementById("main__green-light").style.width = "150px";
+  document.getElementById("main__green-light").style.height = "150px";
+  document.getElementById("main__green-light").style.marginLeft = "auto";
+  document.getElementById("main__green-light").style.marginRight = "auto";
+}, 11000);
+
+setTimeout(() => {
+  document.getElementById("main__green-light").style.border = "solid, green";
+  document.getElementById("main__green-light").style.backgroundColor = "green";
+  document.getElementById("main__green-light").style.borderRadius = "50%";
+  document.getElementById("main__green-light").style.textAlign = "center";
+  document.getElementById("main__green-light").style.width = "150px";
+  document.getElementById("main__green-light").style.height = "150px";
+  document.getElementById("main__green-light").style.marginLeft = "auto";
+  document.getElementById("main__green-light").style.marginRight = "auto";
+}, 11500);
+
+setTimeout(() => {
+  document.getElementById("main__green-light").style.border = "solid, green";
+  document.getElementById("main__green-light").style.backgroundColor = "grey";
+  document.getElementById("main__green-light").style.borderRadius = "50%";
+  document.getElementById("main__green-light").style.textAlign = "center";
+  document.getElementById("main__green-light").style.width = "150px";
+  document.getElementById("main__green-light").style.height = "150px";
+  document.getElementById("main__green-light").style.marginLeft = "auto";
+  document.getElementById("main__green-light").style.marginRight = "auto";
+}, 12000);
+
+setTimeout(() => {
+  document.getElementById("main__green-light").style.border = "solid, green";
+  document.getElementById("main__green-light").style.backgroundColor = "green";
+  document.getElementById("main__green-light").style.borderRadius = "50%";
+  document.getElementById("main__green-light").style.textAlign = "center";
+  document.getElementById("main__green-light").style.width = "150px";
+  document.getElementById("main__green-light").style.height = "150px";
+  document.getElementById("main__green-light").style.marginLeft = "auto";
+  document.getElementById("main__green-light").style.marginRight = "auto";
+}, 12500);
+
+setTimeout(() => {
+  document.getElementById("main__green-light").style.border = "solid, green";
+  document.getElementById("main__green-light").style.backgroundColor = "grey";
+  document.getElementById("main__green-light").style.borderRadius = "50%";
+  document.getElementById("main__green-light").style.textAlign = "center";
+  document.getElementById("main__green-light").style.width = "150px";
+  document.getElementById("main__green-light").style.height = "150px";
+  document.getElementById("main__green-light").style.marginLeft = "auto";
+  document.getElementById("main__green-light").style.marginRight = "auto";
+}, 13000);
+
+setTimeout(() => {
+  document.getElementById("main__red-light").style.border = "solid, red";
+  document.getElementById("main__red-light").style.backgroundColor = "red";
+  document.getElementById("main__red-light").style.borderRadius = "50%";
+  document.getElementById("main__red-light").style.textAlign = "center";
+  document.getElementById("main__red-light").style.width = "150px";
+  document.getElementById("main__red-light").style.height = "150px";
+  document.getElementById("main__red-light").style.marginLeft = "auto";
+  document.getElementById("main__red-light").style.marginRight = "auto";
+
+  document.getElementById("main__yellow-light").style.border = "solid, yellow";
+  document.getElementById("main__yellow-light").style.backgroundColor =
+    "yellow";
+  document.getElementById("main__yellow-light").style.borderRadius = "50%";
+  document.getElementById("main__yellow-light").style.textAlign = "center";
+  document.getElementById("main__yellow-light").style.width = "150px";
+  document.getElementById("main__yellow-light").style.height = "150px";
+  document.getElementById("main__yellow-light").style.marginLeft = "auto";
+  document.getElementById("main__yellow-light").style.marginRight = "auto";
+
+  document.getElementById("main__green-light").style.border = "solid, green";
+  document.getElementById("main__green-light").style.backgroundColor = "green";
+  document.getElementById("main__green-light").style.borderRadius = "50%";
+  document.getElementById("main__green-light").style.textAlign = "center";
+  document.getElementById("main__green-light").style.width = "150px";
+  document.getElementById("main__green-light").style.height = "150px";
+  document.getElementById("main__green-light").style.marginLeft = "auto";
+  document.getElementById("main__green-light").style.marginRight = "auto";
+}, 13500);
